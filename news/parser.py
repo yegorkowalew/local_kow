753a86f35c281140d42692808676e072439164a0
Kowalew Work <kowalew.backup@gmail.com>
2016-07-18 12:19:31 +0300

    В скрипт парсера новостей сайта-провайдера добавлен скрипт для git. В дальнейшем будет два типа новостей. Новости сайта-провайдера и новости разработчика, которые автоматически будут добавляться из git log

--- news/parser.py
+++ news/parser.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from lxml import etree, html
+# from lxml import etree, html
 import lxml.html
 import requests
 from lxml.cssselect import CSSSelector
@@ -8,18 +8,17 @@ from lxml.cssselect import CSSSelector
 r = requests.get('http://www.wimagic.com.ua/ua/news')
 doc = lxml.html.fromstring(r.text)
 
-
-
-    #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div
-    #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div > div > h2
-    #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div > dl > dd
-    #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div > dl > dd > time
-    #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div > d
-    #main > div.items-row.cols-1.row-0.row-fluid.clearfix div div dl dd time
-
 for div in doc.cssselect('div.clearfix'):
 	print(div.cssselect(' div div div h2')[0].text_content().replace('	', '').replace('\n',''))
 	print(div.cssselect(' div div dl dd time')[0].text_content().replace('	', '').replace('\n',''))
-	# print(div.cssselect('time')[0].text_content())
-	# print(div.text_content().replace('	', '').replace('\n',''))
 
+import git
+
+g = git.Git("/home/yegor/local_site/local_kow/")
+log = g.log("--pretty=format:" + '%ad - %s')
+print(log)
+
+
+# git log --pretty=format:"%ad - %s"
+
+# git diff --shortstat краткая статистика по изменениям из последнего коммита
