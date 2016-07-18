abfa0a535ea18c4b3e33ba238fdb0132e9e27f8f
Kowalew Work <kowalew.backup@gmail.com>
2016-07-18 10:47:03 +0300

    Скрипт парсера новостей сайта-провайдера готов. Нужно будет переделать под Django, чтоб заливать новости в базу.

--- news/parser.py
+++ news/parser.py
@@ -1,50 +1,25 @@
 # -*- coding:utf-8 -*-
 
 from lxml import etree, html
-
-f = """
-<div class="items-row cols-1 row-0 row-fluid clearfix">
-	<div class="span12">
-		<div class="item column-1" itemprop="blogPost" itemscope="" itemtype="http://schema.org/BlogPosting">
-			<div class="page-header">
-				<h2 itemprop="name">
-					Підвищення вартості послуг
-				</h2>
-			</div>
-			<dl class="article-info  muted">
-				<dt class="article-info-term">
-					Деталі
-				</dt>
-				<dd class="published">
-					<span class="icon-calendar"></span>
-						<time datetime="2016-03-14T13:00:16+00:00" itemprop="datePublished">14/03/2016</time>
-				</dd>
-			</dl>
-	 			Шановні абоненти!<br>
-	 			<br>
-	 			З 01.04.2016г. компанія «Ведекон» підвищує вартість наданих послуг:<br>
-	 			<br>
-	 			- тарифний план 5 Мбіт/1 Мбіт - 200 грн/міс (з ПДВ)<br>
-	 			<br>
-	 			- тарифний план 2 Мбіт/0,512 Мбіт - 140 грн/міс (з ПДВ)
-		</div>
-			<!-- end item -->
-	</div>
-		<!-- end span -->
-</div>
-"""
+import lxml.html
+import requests
 from lxml.cssselect import CSSSelector
 
-doc=html.fromstring(f)
-# for div in doc.cssselect('div.items-row.cols-1.row-0.row-fluid.clearfix div div div h2'):
-    # print(div.text_content())
+r = requests.get('http://www.wimagic.com.ua/ua/news')
+doc = lxml.html.fromstring(r.text)
+
+
 
     #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div
     #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div > div > h2
     #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div > dl > dd
     #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div > dl > dd > time
     #main > div.blog > div.items-row.cols-1.row-0.row-fluid.clearfix > div > div > d
+    #main > div.items-row.cols-1.row-0.row-fluid.clearfix div div dl dd time
 
-print(doc.cssselect('div.items-row.cols-1.row-0.row-fluid.clearfix div div div h2')[0].text_content())
-print(doc.cssselect('div.items-row.cols-1.row-0.row-fluid.clearfix div div dl dd time')[0].text_content())
-# print(doc.cssselect('div.items-row.cols-1.row-0.row-fluid.clearfix div div dl')[0].text_content())
+for div in doc.cssselect('div.clearfix'):
+	print(div.cssselect(' div div div h2')[0].text_content().replace('	', '').replace('\n',''))
+	print(div.cssselect(' div div dl dd time')[0].text_content().replace('	', '').replace('\n',''))
+	# print(div.cssselect('time')[0].text_content())
+	# print(div.text_content().replace('	', '').replace('\n',''))
+
