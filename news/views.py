36e430926069ca9661a79d2ac59ba8993c290aff
Yegor Kowalew <kowalew.backup@gmail.com>
2016-07-22 22:46:15 +0300

    Начал работать раздел 'Новости'. Новый шаблон news/news.html Сортируются на 'новости разработчика' и 'новости провайдера'. Постраничный вывод

--- news/views.py
+++ news/views.py
@@ -8,6 +8,9 @@ from datetime import datetime
 from news.models import News
 from logs.models import Logs
 from local_site.settings import LOGSTATUSYES, LOGSTATUSNO
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def readd_all_commits(request):
     if request.user.is_superuser:
@@ -15,8 +18,6 @@ def readd_all_commits(request):
             all_commits = go_news_commit()
             News.objects.filter(news_type=2).delete()
             for news_date, news_text in go_news_commit().items():
-                # created_date = datetime.strptime(news_date, '%Y-%m-%d %H:%M:%S')
-                # print(created_date)
                 news_item = News(
                             title = news_text,
                             created_date = datetime.strptime(news_date, '%Y-%m-%d %H:%M:%S'),
@@ -29,16 +30,41 @@ def readd_all_commits(request):
                 'news_commit': go_news_commit(),
             }
             
-            log = Logs(log_user = request.user, log_type = 7, log_status = LOGSTATUSYES,)
+            log = Logs(
+                    log_user = request.user, 
+                    log_type = 7, 
+                    log_status = LOGSTATUSYES,
+                    )
             log.save()
-
+            state = {
+                'state_type':'success',
+                'state_message':'<strong>Отлично!</strong> Все прошло как надо! Добавлено коммитов: %s' % (len(all_commits)),
+                    }
+            data = {
+                'news_commit': all_commits,
+                'state':state,
+                }
             return render(request,
                             'admin/readd_all_commits.html',
                             data,)
-        except:
-            log = Logs(log_user = request.user, log_type = 7, log_status = LOGSTATUSNO,)
+        except Exception as inst:
+            import sys
+            log = Logs(
+                    log_user = request.user, 
+                    log_type = 7, 
+                    log_status = LOGSTATUSNO,
+                    )
             log.save()
-            return HttpResponseRedirect('/')
+            state = {
+                'state_type':'danger',
+                'state_message':inst,
+                    }
+            data  = {
+                'state':state,
+                    }
+            return render(request,
+                            'admin/readd_all_news_wed.html',
+                            data,)
 
     else:
         return HttpResponseRedirect('/')
@@ -56,7 +82,11 @@ def readd_all_news_wed(request):
                             parsed_date = timezone.now(),
                             )
                 news_item.save()
-            log = Logs(log_user = request.user, log_type = 6, log_status = LOGSTATUSYES,)
+            log = Logs(
+                    log_user = request.user, 
+                    log_type = 6, 
+                    log_status = LOGSTATUSYES,
+                    )
             log.save()
             state = {
                 'state_type':'success',
@@ -71,7 +101,11 @@ def readd_all_news_wed(request):
                             data,)
         except Exception as inst:
             import sys
-            log = Logs(log_user = request.user, log_type = 6, log_status = LOGSTATUSNO,)
+            log = Logs(
+                    log_user = request.user, 
+                    log_type = 6, 
+                    log_status = LOGSTATUSNO,
+                    )
             log.save()
             state = {
                 'state_type':'danger',
@@ -85,3 +119,37 @@ def readd_all_news_wed(request):
                             data,)
     else:
         return HttpResponseRedirect('/')
+
+
+def all_news(request, news_type='all'):
+    if news_type=='all':
+        all_news = News.objects.all().order_by('-created_date')
+        news_type = 'Все новости'
+    elif news_type=='commits':
+        all_news = News.objects.filter(news_type=2).order_by('-created_date')
+        news_type = 'Новости разработчика'
+    elif news_type=='wimagic':
+        all_news = News.objects.filter(news_type=1).order_by('-created_date')
+        news_type = 'Новости провайдера'
+    else:
+        return HttpResponseRedirect('/')
+
+    paginator = Paginator(all_news, 8)
+    page = request.GET.get('page')
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    vars = dict(
+        news=news,
+        news_type=news_type,
+        )
+    return render_to_response(
+                    'news/news.html', 
+                    vars, 
+                    context_instance=RequestContext(request)
+                    )
+        # 'commits':News.objects.filter(news_type=2).order_by('-created_date')[:3],
+        # 'news_wed':News.objects.filter(news_type=1).order_by('-created_date')[:3],
