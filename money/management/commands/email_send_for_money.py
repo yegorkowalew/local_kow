9e4a323aac05e4eeffc17b55f4991e9dfd0c1b27
Yegor Work <kowalew.backup@gmail.com>
2016-07-26 17:19:25 +0300

    Работа над отправкой писем. Сортировка пользователей на три группы <50-ти, <20-ти и <0. Осталось создать письмо для каждого пользователя и отправить.

--- money/management/commands/email_send_for_money.py
+++ money/management/commands/email_send_for_money.py
@@ -9,33 +9,50 @@ from userprofile.models import UserProfile
 from datetime import datetime, timedelta
 from django.utils import timezone
 
+def true_log(user, tdelta):
+    ld = Logs.objects.filter(log_user=user, log_type=2).last()
+    if ld == None:
+        return True
+    if ld.log_date + tdelta > timezone.now():
+        return False # отсылаем. 
+    else:
+        return True
+
 def sender_email():
     """
     Нужно чтоб были импортированы:
     UserProfile, User, Post, Logs
     requests, lxml.html
     """
+    zero=[]
+    twenty=[]
+    fifty=[]
     tdelta = timedelta(seconds=50)
-
     for user in User.objects.filter(is_superuser=False):
-        money_last = Post.objects.filter(user=user).last()
-        print(money_last.money)
-        ld = Logs.objects.filter(log_user=user, log_type=2).last()
-        if ld == None:
-            ld = 0
-        # if ld != None:
-        #     if ld.log_date + tdelta > timezone.now():
-        #         print('Еще не прошло время')
-        #     else:
-        #         print('Время вышло')
-
-        if money_last.money < 50 and (ld.log_date + tdelta > timezone.now()):
-            print('надо засылать')
-        
-        if True and False or True:
-            print('test')
-
+        money_last = Post.objects.filter(user=user).last()    
+        if money_last.money < 0 and true_log(user, tdelta):
+            print('zero ',user.username)
+            zero.append(user)
+        elif money_last.money < 20 and true_log(user, tdelta):
+            print('twenty ',user.username)
+            twenty.append(user)
+        elif money_last.money < 50 and true_log(user, tdelta):
+            print('fifty ',user.username)
+            fifty.append(user)
+    messages=[]
+    messages=messages+zero+twenty+fifty
+    print(messages)
     return 'yo'
+
+def switch_case(case):
+    return "You entered " + {
+        '1' : "one",
+        '2' : "two",
+        '3' : "three"
+    }.get(case, "an out of range number")
+
+    num = raw_input("Input a number between 1 and 3: ")
+    print(switch_case(num))
 
 class Command(BaseCommand):
     # Задаём текст помощи, который будет
@@ -44,5 +61,7 @@ class Command(BaseCommand):
     help = 'Auto money check for all users'
 
     def handle(self, *args, **options):
-        sender_email()
+        # sender_email()
+        # switch_case()
+        raw_input("Input a number between 1 and 3: ")
         # print(timedelta(seconds=50))
