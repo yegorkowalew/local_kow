4bbad9c0d121d1ab639f3c58db7bb7fff85811dd
Yegor Kowalew <kowalew.backup@gmail.com>
2016-07-27 07:01:39 +0300

    Работа над отправкой писем. Переделал сортировку пользователей и сборку письма. Нужно разобраться с send_mass_mail()

--- money/management/commands/email_send_for_money.py
+++ money/management/commands/email_send_for_money.py
@@ -8,6 +8,12 @@ from userprofile.models import UserProfile
 # python manage.py email_send_for_money
 from datetime import datetime, timedelta
 from django.utils import timezone
+from number_to_text import num2text
+
+from django.template.loader import get_template
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from local_site.settings import EMAIL_HOST_USER
 
 def true_log(user, tdelta):
     ld = Logs.objects.filter(log_user=user, log_type=2).last()
@@ -20,39 +26,32 @@ def true_log(user, tdelta):
 
 def sender_email():
     """
-    Нужно чтоб были импортированы:
-    UserProfile, User, Post, Logs
-    requests, lxml.html
+    Выбрал пользователей, добавляю каждому пользователю метку о состоянии счета.
+
+
     """
-    zero=[]
-    twenty=[]
-    fifty=[]
     tdelta = timedelta(seconds=50)
-    for user in User.objects.filter(is_superuser=False):
-        money_last = Post.objects.filter(user=user).last()    
-        if money_last.money < 0 and true_log(user, tdelta):
-            print('zero ',user.username)
-            zero.append(user)
-        elif money_last.money < 20 and true_log(user, tdelta):
-            print('twenty ',user.username)
-            twenty.append(user)
-        elif money_last.money < 50 and true_log(user, tdelta):
-            print('fifty ',user.username)
-            fifty.append(user)
-    messages=[]
-    messages=messages+zero+twenty+fifty
-    print(messages)
+    male_units = ((u'день', u'дня', u'дней'), 'm')
+    users = User.objects.filter(is_superuser=False)
+    for user in users:
+        money_last = Post.objects.filter(user=user).order_by('created_date').last()
+        if money_last.money < 50 and true_log(user, tdelta):
+            subject = 'У Вас уже меньше 50грн. на балансе.'
+            tarif_last = Tarif.objects.filter(user=user).last()
+            user.money_state = 'zero'
+            user.subject = subject
+            user.context = {
+                'title': subject, 
+                'pr_user': UserProfile.objects.get(user_id=user.id),
+                'money_last': money_last.money,
+                'tarif_last':tarif_last.money_for_mons/30,
+                'days_left': num2text(round(money_last.money/(tarif_last.money_for_mons/30)), male_units),
+                }
+            # user.txt = render_to_string('email/on_50.txt', context)
+            # user.html = render_to_string('email/on_50.html', context)
+
+
     return 'yo'
-
-def switch_case(case):
-    return "You entered " + {
-        '1' : "one",
-        '2' : "two",
-        '3' : "three"
-    }.get(case, "an out of range number")
-
-    num = raw_input("Input a number between 1 and 3: ")
-    print(switch_case(num))
 
 class Command(BaseCommand):
     # Задаём текст помощи, который будет
@@ -61,7 +60,29 @@ class Command(BaseCommand):
     help = 'Auto money check for all users'
 
     def handle(self, *args, **options):
-        # sender_email()
+        sender_email()
         # switch_case()
-        raw_input("Input a number between 1 and 3: ")
+        # raw_input("Input a number between 1 and 3: ")
         # print(timedelta(seconds=50))
+        # test_users(User.objects.filter(is_superuser=False))
+
+def e_send_on_50(request):
+    subject = 'У Вас уже меньше 50грн. на балансе.'
+    money_last = Post.objects.filter(user=request.user).order_by('created_date').last()
+    tarif_last = Tarif.objects.filter(user=request.user).last()
+    male_units = ((u'день', u'дня', u'дней'), 'm')
+    # (tarif_last.money_for_mons/30)
+    context = {
+        'title': subject, 
+        'pr_user': UserProfile.objects.get(user_id=request.user.id),
+        'money_last': money_last,
+        'tarif_last':tarif_last.money_for_mons/30,
+        'days_left': num2text(round(money_last.money/(tarif_last.money_for_mons/30)), male_units),
+        'request':request,
+        }
+
+    txt = render_to_string('email/on_50.txt', context)
+    html = render_to_string('email/on_50.html', context)
+
+    send_mail(subject, txt, EMAIL_HOST_USER, ['kowalew.backup@gmail.com'], 
+          fail_silently=False, html_message=html)
